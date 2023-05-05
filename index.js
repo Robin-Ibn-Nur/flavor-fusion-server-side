cb048fee489d8e6007e638a84bec269cd08fa65a
saifsammy <saifsammy653@gmail.com>
2023-05-05 14:39:15 +0600

    deploy in vercel

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ app.get('/cafe', (req, res) => {
     res.send(data);
 })
 
-// app.get('/cafe/:id', (req, res) => {
-//     const id = parseInt(req.params?.id);
-//     const selectedRecipes = data?.find(c => c.id == id)
-//     res.send(selectedRecipes)
-// })
+app.get('/cafe/:id', (req, res) => {
+    const id = parseInt(req.params?.id);
+    const selectedRecipes = data?.find(c => c.id == id)
+    res.send(selectedRecipes)
+})
 
 app.listen(Port, () => {
     console.log('server is running on port', Port)
